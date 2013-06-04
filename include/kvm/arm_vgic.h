cf11b0d2f15c8b85a15f1536b359163dd91a8b5d
Marc Zyngier <marc.zyngier@arm.com>
2013-06-04 11:36:38 +0100

    KVM: ARM: vgic: introduce vgic_enable
    Move the code dealing with kicking the VGIC on to vgic_ops.
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- include/kvm/arm_vgic.h
+++ include/kvm/arm_vgic.h
@@ -98,6 +98,7 @@ struct vgic_ops {
 	void	(*clear_underflow)(struct kvm_vcpu *vcpu);
 	void	(*get_vmcr)(struct kvm_vcpu *vcpu, struct vgic_vmcr *vmcr);
 	void	(*set_vmcr)(struct kvm_vcpu *vcpu, struct vgic_vmcr *vmcr);
+	void	(*enable)(struct kvm_vcpu *vcpu);
 };
 
 struct vgic_dist {
