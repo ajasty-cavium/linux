bd44569a6259047ccd196830fd5c5cd39c328f27
Robert Richter <rrichter@cavium.com>
2014-08-04 16:23:53 +0200

    pci, thunder: Fix error handling in thunder_pcie_probe()
    Some resources were not properly removed in case of errors.
    
    Signed-off-by: Robert Richter <rrichter@cavium.com>

--- drivers/pci/host/pci-thunder.c
+++ drivers/pci/host/pci-thunder.c
@@ -159,16 +159,17 @@ static int thunder_pcie_msi_enable(struct thunder_pcie *pcie,
 		return -ENODEV;
 
 	pcie->msi = of_pci_find_msi_chip_by_node(msi_node);
+	if (!pcie->msi)
+		return -ENODEV;
 
-	if (pcie->msi)
-		pcie->msi->dev = pcie->dev;
+	pcie->msi->dev = pcie->dev;
 	bus->msi = pcie->msi;
+
 	return 0;
 }
 
 static int thunder_pcie_probe(struct platform_device *pdev)
 {
-	struct device_node *np = of_node_get(pdev->dev.of_node);
 	struct thunder_pcie *pcie;
 	struct resource *cfg_base;
 	struct pci_host_bridge *bridge;
@@ -176,32 +177,30 @@ static int thunder_pcie_probe(struct platform_device *pdev)
 	int ret;
 
 	pcie = devm_kzalloc(&pdev->dev, sizeof(*pcie), GFP_KERNEL);
-	if (!pcie) {
-		ret = -ENOMEM;
-		goto err;
-	}
+	if (!pcie)
+		return -ENOMEM;
 
+	pcie->node = of_node_get(pdev->dev.of_node);
 	pcie->dev = &pdev->dev;
-	pcie->node = np;
 
 	/* Get controller's configuration space range */
 	cfg_base = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	pcie->cfg_base = devm_ioremap_resource(&pdev->dev, cfg_base);
 	if (IS_ERR(pcie->cfg_base)) {
 		ret = PTR_ERR(pcie->cfg_base);
-		goto err;
+		goto err_ioremap;
 	}
 
 	bridge = of_create_pci_host_bridge(&pdev->dev, &thunder_pcie_ops, pcie);
-	if (IS_ERR_OR_NULL(bridge)) {
+	if (IS_ERR(bridge)) {
 		ret = PTR_ERR(bridge);
-		goto err;
+		goto err_pci;
 	}
 
 	/* Set reference to MSI chip */
 	ret = thunder_pcie_msi_enable(pcie, bridge->bus);
 	if (ret < 0)
-		goto err;
+		goto err_msi;
 
 	platform_set_drvdata(pdev, pcie);
 
@@ -210,8 +209,12 @@ static int thunder_pcie_probe(struct platform_device *pdev)
 	pci_bus_update_busn_res_end(bridge->bus, lastbus);
 
 	return 0;
-err:
-	of_node_put(pdev->dev.of_node);
+err_msi:
+	pci_remove_root_bus(bridge->bus);
+err_pci:
+	devm_ioremap_release(pcie->dev, pcie->cfg_base);
+err_ioremap:
+	of_node_put(pcie->node);
 	kfree(pcie);
 	return ret;
 }
