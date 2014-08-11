08889250dda5b53ad2e0679e70cb520784c0d493
Sunil Goutham <sgoutham@cavium.com>
2014-08-11 16:19:44 +0530

    VNIC: Fixed copyright and license text
    Signed-off-by: Sunil Goutham <sgoutham@cavium.com>

--- drivers/net/ethernet/cavium/thunder/nic_main.c
+++ drivers/net/ethernet/cavium/thunder/nic_main.c
@@ -1,9 +1,10 @@
 /*
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file "COPYING" in the main directory of this archive
- * for more details.
- *
  * Copyright (C) 2014 Cavium, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
  */
 
 #include <linux/module.h>
