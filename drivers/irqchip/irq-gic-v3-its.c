3f62d9c172654830198cf47f2e6be89ec1e4edfa
Radha Mohan Chintakuntla <rchintakuntla@cavium.com>
2014-08-20 14:58:39 -0700

    arm64: gicv3: its: allocate enough size for ITS table
    Allocating a single page for ITS table is not enough. So we now allocate
    based on the values from GITS_TYPER.
    
    Signed-off-by: Radha Mohan Chintakuntla <rchintakuntla@cavium.com>
    Signed-off-by: Robert Richter <rrichter@cavium.com>

--- drivers/irqchip/irq-gic-v3-its.c
+++ drivers/irqchip/irq-gic-v3-its.c
@@ -789,6 +789,7 @@ static int its_alloc_tables(struct its_node *its)
 	int i;
 	int psz = PAGE_SIZE;
 	u64 shr = GITS_BASER_InnerShareable;
+	u64 typer = readq_relaxed(its->base + GITS_TYPER);
 
 	for (i = 0; i < GITS_BASER_NR_REGS; i++) {
 		u64 val = readq_relaxed(its->base + GITS_BASER + i * 8);
@@ -796,12 +797,16 @@ static int its_alloc_tables(struct its_node *its)
 		u64 entry_size = GITS_BASER_ENTRY_SIZE(val);
 		u64 tmp;
 		void *base;
+		u64 max_devices, max_ittsize;
 
 		if (type == GITS_BASER_TYPE_NONE)
 			continue;
 
-		/* We're lazy and only allocate a single page for now */
-		base = (void *)get_zeroed_page(GFP_KERNEL);
+		max_devices = 1ULL << (((typer >> 13) & 0x1f) + 1);
+		max_ittsize = ((typer >> 4) & 0xf) + 1;
+		max_ittsize *= max_devices;
+
+		base = (void *)__get_free_pages(GFP_KERNEL | __GFP_ZERO, get_order(max_ittsize));
 		if (!base) {
 			err = -ENOMEM;
 			goto out_free;
@@ -829,7 +834,7 @@ retry_baser:
 			break;
 		}
 
-		val |= (PAGE_SIZE / psz) - 1;
+		val |= (max_ittsize / psz) - 1;
 
 		writeq_relaxed(val, its->base + GITS_BASER + i * 8);
 		tmp = readq_relaxed(its->base + GITS_BASER + i * 8);
