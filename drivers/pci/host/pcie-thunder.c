0d907d2e2d15c897098815a1ee4b9a77bb3941cf
Robert Richter <rrichter@cavium.com>
2014-09-22 18:13:03 +0200

    thunder: Fix copyright year
    Signed-off-by: Robert Richter <rrichter@cavium.com>

--- drivers/pci/host/pcie-thunder.c
+++ drivers/pci/host/pcie-thunder.c
@@ -1,7 +1,7 @@
 /*
  * PCIe host controller driver for Cavium Thunder SOC
  *
- * Copyright (C) 2013, Cavium Inc.
+ * Copyright (C) 2014, Cavium Inc.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
