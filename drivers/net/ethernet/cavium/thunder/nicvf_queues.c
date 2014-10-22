265f72215986569295f8a6b5cba3c56a73efdc0d
Robert Richter <rrichter@cavium.com>
2014-10-22 13:20:04 +0200

    VNIC: Fix memory allocation for generic allocator
    After updating to v3.18-rc1 a problem occurred with memory allocation
    while testing them:
    
     thunder-nicvf 0002:01:00.1 eth0: Unable to allocate memory for send queue
    
    Bisecting it found that reverting the follwing patch solves the issue:
    
    commit d4932f9e81ae7a7bf3c3967e48373909b9c98ee5
    Author: Laura Abbott <lauraa@codeaurora.org>
    Date:   Thu Oct 9 15:26:44 2014 -0700
    
        arm64: add atomic pool for non-coherent and CMA allocations
    
        Neither CMA nor noncoherent allocations support atomic allocations.
        Add a dedicated atomic pool to support this.
    
        Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
        Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
        Cc: Arnd Bergmann <arnd@arndb.de>
        Cc: David Riley <davidriley@chromium.org>
        Cc: Olof Johansson <olof@lixom.net>
        Cc: Ritesh Harjain <ritesh.harjani@gmail.com>
        Cc: Russell King <linux@arm.linux.org.uk>
        Cc: Thierry Reding <thierry.reding@gmail.com>
        Cc: Will Deacon <will.deacon@arm.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    This patch was introduced with:
    
     [PATCHv7 0/5] DMA Atomic pool for arm64
     https://lkml.org/lkml/2014/8/11/609
    
    and implements generic memory allocation for arm64.
    
    The new implementation causes dma_alloc_coherent() to return an error.
    Looking at the code a __GFP_WAIT check was added causing the (no-wait)
    GFP_ATOMIC request from nicvf_alloc_q_desc_mem() to fail. Since the
    function is called from process context, the flag can be modified to
    use GFP_KERNEL. This patch fixes the problem.
    
    Signed-off-by: Robert Richter <rrichter@cavium.com>

--- drivers/net/ethernet/cavium/thunder/nicvf_queues.c
+++ drivers/net/ethernet/cavium/thunder/nicvf_queues.c
@@ -23,7 +23,7 @@ static int nicvf_alloc_q_desc_mem(struct nicvf *nic, struct q_desc_mem *dmem,
 	dmem->q_len = q_len;
 	dmem->size = (desc_size * q_len) + align_bytes;
 	dmem->unalign_base = dma_alloc_coherent(&nic->pdev->dev, dmem->size,
-						&dmem->dma, GFP_ATOMIC);
+						&dmem->dma, GFP_KERNEL);
 	if (!dmem->unalign_base)
 		return -1;
 
