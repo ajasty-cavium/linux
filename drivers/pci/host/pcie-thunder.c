32a5a5935f38fb774d2518f500d004c2fd5428d2
TIRUMALESH CHALAMARLA <tchalamarla@cavium.com>
2014-11-12 22:32:00 -0500

    /tmp/pcie.patch
    Signed-off-by: TIRUMALESH CHALAMARLA <tchalamarla@cavium.com>
    Signed-off-by: Robert Richter <rrichter@cavium.com>

--- drivers/pci/host/pcie-thunder.c
+++ drivers/pci/host/pcie-thunder.c
@@ -23,11 +23,17 @@
 #define THUNDER_PCIE_DEV_SHIFT		15
 #define THUNDER_PCIE_FUNC_SHIFT		12
 
+#define THUNDER_ECAM0_CFG_BASE		0x848000000000
+#define THUNDER_ECAM1_CFG_BASE		0x849000000000
+#define THUNDER_ECAM2_CFG_BASE		0x84a000000000
+#define THUNDER_ECAM3_CFG_BASE		0x84b000000000
+
 struct thunder_pcie {
 	struct device_node	*node;
 	struct device		*dev;
 	void __iomem		*cfg_base;
 	struct msi_chip		*msi;
+	int 			ecam;
 };
 
 /*
@@ -78,6 +84,108 @@ static void pci_dev_resource_fixup(struct pci_dev *dev)
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_CAVIUM, PCI_ANY_ID,
 			pci_dev_resource_fixup);
 
+static int thunder_pcie_check_cfg_access(int ecam, unsigned int bus,
+					 unsigned int devfn)
+{
+	int supported = 0;
+	uint16_t bdf = (bus << 8) | devfn;
+
+	if (ecam == 0) {
+		switch (bdf) {
+			case 0x008:   /* RSL bridge */
+			case 0x010:   /* SMMU */
+			case 0x030:   /* GPIO */
+			case 0x038:   /* MPI */
+			case 0x0A0:   /* RAD bridge */
+			case 0x0A8:   /* ZIP bridge */
+			case 0x0B0:   /* DFA bridge */
+			case 0x100:   /* MRML */
+			case 0x101:   /* RST */
+			case 0x103:   /* FUS */
+			case 0x104:   /* FUSF */
+			case 0x109:   /* L2C */
+			case 0x10A:   /* SGPIO */
+			case 0x10C:   /* EMM */
+			case 0x10D:   /* KEY */
+			case 0x10e:   /* MIO_BOOT */
+			case 0x180:   /* BGX0 */
+			case 0x181:   /* BGX1 */
+			case 0x1E2:   /* GSER2 */
+			case 0x1E3:   /* GSER3 */
+			case 0x1E6:   /* GSER6 */
+			case 0x1E7:   /* GSER7 */
+				supported = 1;
+				break;
+			default:
+				supported = 0;
+		}
+	} else if (ecam == 1) {
+		switch (bdf) {
+			case 0x008:   /* SMMU */
+			case 0x020:   /* AHCI0*/
+			//case 0x028:   /* AHCI1 */
+			//case 0x030:   /* AHCI2 */
+			//case 0x038:   /* AHCI3 */
+			//case 0x040:   /* AHCI4 */
+			//case 0x048:   /* AHCI5 */
+			//case 0x050:   /* AHCI5 */
+			//case 0x058:   /* AHCI7 */
+			//case 0x080:   /* PCIRC0 */
+			//case 0x098:   /* PCIRC1 */
+			//case 0x0A8:   /* PCIRC2 */
+				supported = 1;
+				break;
+			default:
+				supported = 0;
+		}
+	} else if (ecam == 2) {
+		switch (bdf) {
+			case 0x008:   /* SMMU */
+			case 0x010:   /* NIC Bridge */
+			case 0x100:   /* NIC PF */
+			case 0x101:   /* NIC VF */
+			case 0x102:   /* NIC VF */
+			case 0x103:   /* NIC VF */
+			case 0x104:   /* NIC VF */
+			case 0x105:   /* NIC VF */
+			case 0x106:   /* NIC VF */
+			case 0x107:   /* NIC VF */
+			case 0x108:   /* NIC VF */
+			case 0x109:   /* NIC VF */
+			case 0x10A:   /* NIC VF */
+			case 0x10B:   /* NIC VF */
+			case 0x10C:   /* NIC VF */
+			case 0x10D:   /* NIC VF */
+			case 0x10E:   /* NIC VF */
+			case 0x110:   /* NIC VF */
+				supported = 1;
+				break;
+			default:
+				supported = 0;
+		}
+	} else if (ecam == 3) {
+		switch (bdf) {
+			case 0x008:   /* SMMU */
+		//	case 0x018:   /* AHCI8 */
+		//	case 0x020:   /* AHCI9*/
+		//	case 0x028:   /* AHCI10 */
+		//	case 0x030:   /* AHCI11 */
+		//	case 0x038:   /* AHCI12 */
+		//	case 0x040:   /* AHCI13 */
+		//	case 0x048:   /* AHCI14 */
+		//	case 0x050:   /* AHCI15 */
+			//case 0x080:   /* PCIRC3 */
+			//case 0x098:   /* PCIRC4 */
+			//case 0x0A8:   /* PCIRC5 */
+				supported = 1;
+				break;
+			default:
+				supported = 0;
+		}
+	}
+	return supported;
+}
+
 static void __iomem *thunder_pcie_cfg_base(struct thunder_pcie *pcie,
 				 unsigned int bus, unsigned int devfn)
 {
@@ -92,21 +200,33 @@ static int thunder_pcie_read_config(struct pci_bus *bus, unsigned int devfn,
 	struct thunder_pcie *pcie = bus->sysdata;
 	void __iomem *addr;
 	unsigned int busnr = bus->number;
+	int supported;
 
 	if (busnr > 255 || devfn > 255 || reg > 4095)
 		return PCIBIOS_DEVICE_NOT_FOUND;
 
 	addr = thunder_pcie_cfg_base(pcie, busnr, devfn) + reg;
 
+	supported = thunder_pcie_check_cfg_access(pcie->ecam, busnr, devfn);
+
 	switch (size) {
 	case 1:
-		*val = readb(addr);
+		if (!supported) 
+			*val = 0xff;
+		else 
+			*val = readb(addr);
 		break;
 	case 2:
-		*val = readw(addr);
+		if (!supported)
+			*val = 0xffff;
+		else 
+			*val = readw(addr);
 		break;
 	case 4:
-		*val = readl(addr);
+		if (!supported)
+			*val = 0xffffffff;
+		else 
+			*val = readl(addr);
 		break;
 	default:
 		return PCIBIOS_BAD_REGISTER_NUMBER;
@@ -185,6 +305,22 @@ static int thunder_pcie_probe(struct platform_device *pdev)
 
 	/* Get controller's configuration space range */
 	cfg_base = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	switch(cfg_base->start) {
+		case THUNDER_ECAM0_CFG_BASE:
+			pcie->ecam = 0;
+			break;
+		case THUNDER_ECAM1_CFG_BASE:
+			pcie->ecam = 1;
+			break;
+		case THUNDER_ECAM2_CFG_BASE:
+			pcie->ecam = 2;
+			break;
+		case THUNDER_ECAM3_CFG_BASE:
+			pcie->ecam = 3;
+			break;
+	}
+	pr_err("%s: ECAM%d CFG BASE 0x%llx\n",__func__, pcie->ecam, (uint64_t)cfg_base->start);
+
 	pcie->cfg_base = devm_ioremap_resource(&pdev->dev, cfg_base);
 	if (IS_ERR(pcie->cfg_base)) {
 		ret = PTR_ERR(pcie->cfg_base);
