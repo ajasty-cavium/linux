65c602a4f33dca96b48a617a9c5c9c17205ff633
Andre Przywara <andre.przywara@arm.com>
2014-12-08 11:35:10 +0000

    improve comment for reset_mpidr()
    git url of original branch: git://linux-arm.org/linux-ap.git  -b kvm-gicv3/v4
    (cherry picked from commit 6d2cc20ebdf2e9e31b9948849e03ccaf99202f4f)
    Signed-off-by: Tirumalesh Chalamarla <tchalamarla@cavium.com>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -276,9 +276,11 @@ static void reset_mpidr(struct kvm_vcpu *vcpu, const struct sys_reg_desc *r)
 	u64 mpidr;
 
 	/*
-	 * Map the vcpu_id into the first three Aff fields of the MPIDR.
-	 * We limit the number of VCPUs in Aff0 due to a limitation in the
-	 * ICC_SGIxR registers of the GICv3.
+	 * Map the vcpu_id into the first three affinity level fields of
+	 * the MPIDR. We limit the number of VCPUs in level 0 due to a
+	 * limitation to 16 CPUs in that level in the ICC_SGIxR registers
+	 * of the GICv3 to be able to address each CPU directly when
+	 * sending IPIs.
 	 */
 	mpidr = (vcpu->vcpu_id & 0x0f) << MPIDR_LEVEL_SHIFT(0);
 	mpidr |= ((vcpu->vcpu_id >> 4) & 0xff) << MPIDR_LEVEL_SHIFT(1);
