a2680a446efda729cde159f673e61238d56ba5fc
Robert Richter <rrichter@cavium.com>
2015-02-04 20:56:11 +0100

    arm64, gic-v3: Fixing whitespaces in gic_read_iar()
    Signed-off-by: Robert Richter <rrichter@cavium.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -113,14 +113,14 @@ static u64 __maybe_unused gic_read_iar(void)
 {
 	u64 irqstat;
 
-    /* Workaround for thunder replay issue */
-    asm volatile("nop;nop;nop;nop;");
-    asm volatile("nop;nop;nop;nop;");
-    asm volatile("mrs_s %0, " __stringify(ICC_IAR1_EL1) : "=r" (irqstat));
+	/* Workaround for thunder replay issue */
+	asm volatile("nop;nop;nop;nop;");
+	asm volatile("nop;nop;nop;nop;");
+	asm volatile("mrs_s %0, " __stringify(ICC_IAR1_EL1) : "=r" (irqstat));
 #ifdef CONFIG_THUNDERX_PASS1_ERRATA_23154
-    asm volatile("nop;nop;nop;nop;");
+	asm volatile("nop;nop;nop;nop;");
 #endif
-    mb();
+	mb();
 	return irqstat;
 }
 
