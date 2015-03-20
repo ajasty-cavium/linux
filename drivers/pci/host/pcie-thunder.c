aa5865aba15b4d29074566deda4a2ea8f748e33f
Radha Mohan Chintakuntla <rchintakuntla@cavium.com>
2015-03-20 15:40:38 -0700

    pci, thunderx: Add I2C devfn cases for node1
    Add I2C devfn switch cases for node1.
    
    Signed-off-by: Radha Mohan Chintakuntla <rchintakuntla@cavium.com>

--- drivers/pci/host/pcie-thunder.c
+++ drivers/pci/host/pcie-thunder.c
@@ -399,6 +399,13 @@ static int thunder_pcie_check_ecam_cfg_access(int ecam, unsigned int bus,
 		case 0x10C:   /* EMM */
 		case 0x10D:   /* KEY */
 		case 0x10e:   /* MIO_BOOT */
+		case 0x148:   /* TWSI0 */
+		case 0x149:   /* TWSI1 */
+		case 0x14A:   /* TWSI2 */
+		case 0x14B:   /* TWSI3 */
+		case 0x14C:   /* TWSI4 */
+		case 0x14D:   /* TWSI5 */
+
 			supported = 1;
 			break;
 		case 0x180:   /* BGX0 */
