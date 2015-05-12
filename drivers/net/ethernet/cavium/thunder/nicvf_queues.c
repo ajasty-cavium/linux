e7eb482db1cd791bf32140f4df2897883731b7d0
Sunil Goutham <sgoutham@cavium.com>
2015-05-12 13:13:02 +0530

    VNIC: Set RPS mapping only to local CPUs of a node
    It is observed that if interface is on node1 and packet processing
    is done on node0 then overall receive performance drops almost
    to 1/3rd of what is achieved if both interrupt and packet processing
    are handled on same node.
    
    Signed-off-by: Sunil Goutham <sgoutham@cavium.com>

--- drivers/net/ethernet/cavium/thunder/nicvf_queues.c
+++ drivers/net/ethernet/cavium/thunder/nicvf_queues.c
@@ -519,7 +519,7 @@ static void nicvf_rcv_queue_config(struct nicvf *nic, struct queue_set *qs,
 #ifdef CONFIG_RPS
 	cq = &qs->cq[qidx];
 	/* Set RPS CPU map */
-	cpumask_copy(rps_mask, cpu_online_mask);
+	cpumask_copy(rps_mask, cpumask_of_node(nic->node));
 	cpumask_clear_cpu(cpumask_first(&cq->affinity_mask), rps_mask);
 
 	rxqueue = nic->netdev->_rx + qidx;
