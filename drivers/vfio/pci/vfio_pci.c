fc5ebe8bb2a4853987a024801c56647e96ff2e8e
Tirumalesh CHalamarla <tchalamarla.cavium.com>
2015-05-21 11:59:48 -0700

    VFIO: Remove ITS emulation msix hacks from VFIO
    now that we are planning to move the misx handling to ITS driver,
    remove this hacks from here.
    
    Signed-off-by: Tirumalesh CHalamarla <tchalamarla.cavium.com>

--- drivers/vfio/pci/vfio_pci.c
+++ drivers/vfio/pci/vfio_pci.c
@@ -47,7 +47,6 @@ static int vfio_pci_enable(struct vfio_pci_device *vdev)
 	int ret;
 	u16 cmd;
 	u8 msix_pos;
-	struct msix_entry entry[6] = {{0, 0}, {0, 1} };
 
 	/* Don't allow our initial saved state to include busmaster */
 	pci_clear_master(pdev);
@@ -91,8 +90,6 @@ static int vfio_pci_enable(struct vfio_pci_device *vdev)
 		vdev->msix_bar = table & PCI_MSIX_TABLE_BIR;
 		vdev->msix_offset = table & PCI_MSIX_TABLE_OFFSET;
 		vdev->msix_size = ((flags & PCI_MSIX_FLAGS_QSIZE) + 1) * 16;
-		/* this is a hack to enable misx field in ECAM */
-		pci_enable_msix_range(pdev, entry, 0, 1);
 	} else
 		vdev->msix_bar = 0xFF;
 
@@ -164,7 +161,6 @@ static void vfio_pci_disable(struct vfio_pci_device *vdev)
 		else
 			vdev->needs_reset = false;
 	}
-	pci_disable_msix(pdev);
 
 	pci_restore_state(pdev);
 out:
@@ -798,7 +794,6 @@ static int vfio_pci_mmap(void *device_data, struct vm_area_struct *vma)
 		return -EINVAL;
 
 	if (index == vdev->msix_bar) {
-#if 0
 		/*
 		 * Disallow mmaps overlapping the MSI-X table; users don't
 		 * get to touch this directly.  We could find somewhere
@@ -812,7 +807,6 @@ static int vfio_pci_mmap(void *device_data, struct vm_area_struct *vma)
 		if (!(req_start >= vdev->msix_offset + vdev->msix_size ||
 		      req_start + req_len <= vdev->msix_offset))
 			return -EINVAL;
-#endif
 	}
 
 	/*
