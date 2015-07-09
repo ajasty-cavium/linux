a5b6b29c9f574935e2db8c4ed45c73f96feed184
David Daney <david.daney@cavium.com>
2015-07-09 10:32:27 -0700

    pci, pcie-thunder: Simplify and use generic hooks.
    Signed-off-by: David Daney <david.daney@cavium.com>

--- drivers/pci/host/pcie-thunder.c
+++ drivers/pci/host/pcie-thunder.c
@@ -1,24 +1,23 @@
 /*
  * PCIe host controller driver for Cavium Thunder SOC
  *
- * Copyright (C) 2014, Cavium Inc.
+ * Copyright (C) 2014, 2015 Cavium Inc.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
  */
-/* #define DEBUG 1 */
-#include <linux/delay.h>
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mmconfig.h>
-#include <linux/interrupt.h>
 #include <linux/of_irq.h>
 #include <linux/of_pci.h>
 #include <linux/pci.h>
 #include <linux/platform_device.h>
 #include <linux/msi.h>
+#include <linux/irqchip/arm-gic-v3.h>
 
 #define PCI_DEVICE_ID_THUNDER_BRIDGE	0xa002
 
@@ -45,8 +44,8 @@
 #define THUNDER_GSER_CFG_BGX		(1ULL << 2)
 #define THUNDER_GSER_CFG_SATA		(1ULL << 5)
 
-void __iomem *gser_base0;
-void __iomem *gser_base1;
+static void __iomem *gser_base0;
+static void __iomem *gser_base1;
 
 struct thunder_pcie {
 	struct device_node	*node;
@@ -61,24 +60,23 @@ int thunder_pem_requester_id(struct pci_dev *dev);
 
 static atomic_t thunder_pcie_ecam_probed;
 
-static int pci_requester_id_ecam(struct pci_dev *dev)
+static u32 pci_requester_id_ecam(struct pci_dev *dev)
 {
 	return (((pci_domain_nr(dev->bus) >> 2) << 19) |
 		((pci_domain_nr(dev->bus) % 4) << 16) |
 		(dev->bus->number << 8) | dev->devfn);
 }
 
-int pci_requester_id(struct pci_dev *dev)
+static u32 thunder_pci_requester_id(struct pci_dev *dev, u16 alias)
 {
 	int ret;
 
 	ret = thunder_pem_requester_id(dev);
 	if (ret >= 0)
-		return ret;
+		return (u32)ret;
 
 	return pci_requester_id_ecam(dev);
 }
-EXPORT_SYMBOL(pci_requester_id);
 
 /*
  * This bridge is just for the sake of supporting ARI for
@@ -140,7 +138,7 @@ static void __iomem *thunder_get_gser_base(int node, int qlm)
 {
 	void __iomem *base;
 
-	if (!node)
+	if (node == 0)
 		base = gser_base0;
 	else
 		base = gser_base1;
@@ -153,14 +151,16 @@ static u64 thunder_get_gser_cfg(int node, int qlm)
 	return readq(thunder_get_gser_base(node, qlm) + THUNDER_GSER_CFG);
 }
 
-static int thunder_pcie_check_ecam_cfg_access(int ecam, unsigned int bus,
-					unsigned int devfn)
+static bool thunder_pcie_check_ecam_cfg_access(int ecam, unsigned int bus,
+					       unsigned int devfn)
 {
-	int supported = 0;
+	bool supported = false;
 	u16 bdf = (bus << 8) | devfn;
 	u64 gser_cfg;
+	int node = ecam >> 2;
+	int instance = ecam & 3;
 
-	if (ecam == 0) {
+	if (instance == 0) {
 		switch (bdf) {
 		case 0x008:   /* RSL bridge */
 		case 0x010:   /* SMMU */
@@ -168,97 +168,89 @@ static int thunder_pcie_check_ecam_cfg_access(int ecam, unsigned int bus,
 		case 0x038:   /* MPI */
 		case 0x080:   /* USB0 */
 		case 0x088:   /* USB1 */
-		case 0x0A0:   /* RAD bridge */
-		case 0x0A8:   /* ZIP bridge */
-		case 0x0B0:   /* DFA bridge */
+		case 0x0a0:   /* RAD bridge */
+		case 0x0a8:   /* ZIP bridge */
+		case 0x0b0:   /* DFA bridge */
 		case 0x100:   /* MRML */
 		case 0x101:   /* RST */
 		case 0x103:   /* FUS */
 		case 0x104:   /* FUSF */
-		case 0x105:   /* OCX  */
+		case 0x105:   /* OCX */
+		case 0x108:   /* NCSI */
 		case 0x109:   /* L2C */
-		case 0x10A:   /* SGPIO */
-		case 0x10C:   /* EMM */
-		case 0x10D:   /* KEY */
+		case 0x10a:   /* SGPIO */
+		case 0x10b:   /* SMI */
+		case 0x10c:   /* EMM */
+		case 0x10d:   /* KEY */
 		case 0x10e:   /* MIO_BOOT */
 		case 0x130 ... 0x13f: /* L2C */
 		case 0x140 ... 0x141: /* UART */
-		case 0x144 ... 0x145: /* UART */
-		case 0x148:   /* TWSI0 */
-		case 0x149:   /* TWSI1 */
-		case 0x14A:   /* TWSI2 */
-		case 0x14B:   /* TWSI3 */
-		case 0x14C:   /* TWSI4 */
-		case 0x14D:   /* TWSI5 */
+		case 0x144 ... 0x145: /* VRM */
+		case 0x148 ... 0x14d: /* TWSI0 - TWSI5 */
 
 		case 0x150 ... 0x153: /* LMC */
 		case 0x158 ... 0x159: /* IOBN */
 		case 0x160 ... 0x163: /* OCLA */
 
-		case 0x170:   /* PEM0 */
-		case 0x171:   /* PEM1 */
-		case 0x172:   /* PEM2 */
-		case 0x173:   /* PEM3 */
-		case 0x174:   /* PEM4 */
-		case 0x175:   /* PEM5 */
+		case 0x170 ... 0x175: /* PEM0 - PEM5 */
 
 		case 0x200:   /* RAD */
 		case 0x300:   /* ZIP */
 		case 0x400:   /* HFA */
-			supported = 1;
+			supported = true;
 			break;
 
 		case 0x180:   /* BGX0 */
-			gser_cfg = thunder_get_gser_cfg(0, 0);
+			gser_cfg = thunder_get_gser_cfg(node, 0);
 			if (gser_cfg & THUNDER_GSER_CFG_BGX)
-				supported = 1;
+				supported = true;
 			break;
 		case 0x181:   /* BGX1 */
-			gser_cfg = thunder_get_gser_cfg(0, 1);
+			gser_cfg = thunder_get_gser_cfg(node, 1);
 			if (gser_cfg & THUNDER_GSER_CFG_BGX)
-				supported = 1;
+				supported = true;
 			break;
 		default:
-			supported = 0;
+			break;
 		}
-	} else if (ecam == 1) {
+	} else if (instance == 1) {
 		switch (bdf) {
 		case 0x008:   /* SMMU */
 		case 0x020:   /* AHCI0*/
 		case 0x028:   /* AHCI1 */
 		case 0x030:   /* AHCI2 */
 		case 0x038:   /* AHCI3 */
-			gser_cfg = thunder_get_gser_cfg(0, 2);
+			gser_cfg = thunder_get_gser_cfg(node, 2);
 			if (gser_cfg & THUNDER_GSER_CFG_SATA)
-				supported = 1;
+				supported = true;
 			break;
 		case 0x040:   /* AHCI4 */
 		case 0x048:   /* AHCI5 */
 		case 0x050:   /* AHCI5 */
 		case 0x058:   /* AHCI7 */
-			gser_cfg = thunder_get_gser_cfg(0, 3);
+			gser_cfg = thunder_get_gser_cfg(node, 3);
 			if (gser_cfg & THUNDER_GSER_CFG_SATA)
-				supported = 1;
+				supported = true;
 			break;
 		case 0x080:   /* PCIRC0 */
 		case 0x090:   /* PCIRC1 */
-		case 0x0A0:   /* PCIRC2 */
+		case 0x0a0:   /* PCIRC2 */
 		default:
-			supported = 0;
+			break;
 		}
-	} else if (ecam == 2) {
+	} else if (instance == 2) {
 		switch (bdf) {
 		case 0x008:   /* SMMU */
 		case 0x010:   /* NIC Bridge */
 		case 0x018:   /* TNS */
 		case 0x100:   /* NIC PF */
 		case 0x101 ... 0x180:   /* NIC VF */
-			supported = 1;
+			supported = true;
 			break;
 		default:
-			supported = 0;
+			break;
 		}
-	} else if (ecam == 3) {
+	} else if (instance == 3) {
 		switch (bdf) {
 		case 0x008:   /* SMMU */
 			supported = 1;
@@ -267,149 +259,23 @@ static int thunder_pcie_check_ecam_cfg_access(int ecam, unsigned int bus,
 		case 0x028:   /* AHCI9*/
 		case 0x030:   /* AHCI10 */
 		case 0x038:   /* AHCI11 */
-			gser_cfg = thunder_get_gser_cfg(0, 6);
+			gser_cfg = thunder_get_gser_cfg(node, 6);
 			if (gser_cfg & THUNDER_GSER_CFG_SATA)
-				supported = 1;
+				supported = true;
 			break;
 		case 0x040:   /* AHCI12 */
 		case 0x048:   /* AHCI13 */
 		case 0x050:   /* AHCI14 */
 		case 0x058:   /* AHCI15 */
-			gser_cfg = thunder_get_gser_cfg(0, 7);
+			gser_cfg = thunder_get_gser_cfg(node, 7);
 			if (gser_cfg & THUNDER_GSER_CFG_SATA)
-				supported = 1;
+				supported = true;
 			break;
 		case 0x080:   /* PCIRC3 */
 		case 0x090:   /* PCIRC4 */
-		case 0x0A0:   /* PCIRC5 */
+		case 0x0a0:   /* PCIRC5 */
 		default:
-			supported = 0;
-		}
-	}
-
-	if (ecam == 4) {
-		switch (bdf) {
-		case 0x008:   /* RSL bridge */
-		case 0x010:   /* SMMU */
-		case 0x030:   /* GPIO */
-		case 0x038:   /* MPI */
-		case 0x080:   /* USB0 */
-		case 0x088:   /* USB1 */
-		case 0x0A0:   /* RAD bridge */
-		case 0x0A8:   /* ZIP bridge */
-		case 0x0B0:   /* DFA bridge */
-		case 0x100:   /* MRML */
-		case 0x101:   /* RST */
-		case 0x103:   /* FUS */
-		case 0x104:   /* FUSF */
-		case 0x105:   /* OCX  */
-		case 0x109:   /* L2C */
-		case 0x10A:   /* SGPIO */
-		case 0x10C:   /* EMM */
-		case 0x10D:   /* KEY */
-		case 0x10e:   /* MIO_BOOT */
-		case 0x130 ... 0x13f: /* L2C */
-		case 0x140 ... 0x141: /* UART */
-		case 0x144 ... 0x145: /* UART */
-		case 0x148:   /* TWSI0 */
-		case 0x149:   /* TWSI1 */
-		case 0x14A:   /* TWSI2 */
-		case 0x14B:   /* TWSI3 */
-		case 0x14C:   /* TWSI4 */
-		case 0x14D:   /* TWSI5 */
-
-		case 0x150 ... 0x153: /* LMC */
-		case 0x158 ... 0x159: /* IOBN */
-		case 0x160 ... 0x163: /* OCLA */
-
-		case 0x170:   /* PEM0 */
-		case 0x171:   /* PEM1 */
-		case 0x172:   /* PEM2 */
-		case 0x173:   /* PEM3 */
-		case 0x174:   /* PEM4 */
-		case 0x175:   /* PEM5 */
-
-		case 0x200:   /* RAD */
-		case 0x300:   /* ZIP */
-		case 0x400:   /* HFA */
-			supported = 1;
 			break;
-		case 0x180:   /* BGX0 */
-			gser_cfg = thunder_get_gser_cfg(1, 0);
-			if (gser_cfg & THUNDER_GSER_CFG_BGX)
-				supported = 1;
-			break;
-		case 0x181:   /* BGX1 */
-			gser_cfg = thunder_get_gser_cfg(1, 1);
-			if (gser_cfg & THUNDER_GSER_CFG_BGX)
-				supported = 1;
-			break;
-		default:
-			supported = 0;
-		}
-	} else if (ecam == 5) {
-		switch (bdf) {
-		case 0x008:   /* SMMU */
-		case 0x020:   /* AHCI0*/
-		case 0x028:   /* AHCI1 */
-		case 0x030:   /* AHCI2 */
-		case 0x038:   /* AHCI3 */
-			gser_cfg = thunder_get_gser_cfg(1, 2);
-			if (gser_cfg & THUNDER_GSER_CFG_SATA)
-				supported = 1;
-			break;
-		case 0x040:   /* AHCI4 */
-		case 0x048:   /* AHCI5 */
-		case 0x050:   /* AHCI5 */
-		case 0x058:   /* AHCI7 */
-			gser_cfg = thunder_get_gser_cfg(1, 3);
-			if (gser_cfg & THUNDER_GSER_CFG_SATA)
-				supported = 1;
-			break;
-		case 0x080:   /* PCIRC0 */
-		case 0x090:   /* PCIRC1 */
-		case 0x0A0:   /* PCIRC2 */
-		default:
-			supported = 0;
-		}
-	} else if (ecam == 6) {
-		switch (bdf) {
-		case 0x008:   /* SMMU */
-		case 0x010:   /* NIC Bridge */
-		case 0x100:   /* NIC PF */
-		case 0x101:   /* NIC VF */
-		case 0x102 ... 0x180:   /* NIC VF */
-			supported = 1;
-			break;
-		default:
-			supported = 0;
-		}
-	} else if (ecam == 7) {
-		switch (bdf) {
-		case 0x008:   /* SMMU */
-			supported = 1;
-			break;
-		case 0x020:   /* AHCI8 */
-		case 0x028:   /* AHCI9*/
-		case 0x030:   /* AHCI10 */
-		case 0x038:   /* AHCI11 */
-			gser_cfg = thunder_get_gser_cfg(1, 6);
-			if (gser_cfg & THUNDER_GSER_CFG_SATA)
-				supported = 1;
-			break;
-		case 0x040:   /* AHCI12 */
-		case 0x048:   /* AHCI13 */
-		case 0x050:   /* AHCI14 */
-		case 0x058:   /* AHCI15 */
-			gser_cfg = thunder_get_gser_cfg(1, 7);
-			if (gser_cfg & THUNDER_GSER_CFG_SATA)
-				supported = 1;
-			break;
-		case 0x080:   /* PCIRC3 */
-		case 0x090:   /* PCIRC4 */
-		case 0x0A0:   /* PCIRC5 */
-		default:
-			supported = 0;
 		}
 	}
 
@@ -509,16 +375,10 @@ static struct pci_ops thunder_pcie_ops = {
 	.write	= thunder_pcie_write_config,
 };
 
-#ifdef CONFIG_KVM_ARM_VGIC
-struct msi_chip *vgic_its_get_msi_node(struct pci_bus *bus,
-						struct msi_chip *msi);
-#endif
-
 static int thunder_pcie_msi_enable(struct thunder_pcie *pcie,
 					struct pci_bus *bus)
 {
 	struct device_node *msi_node;
-	struct msi_chip *vits_msi;
 
 	msi_node = of_parse_phandle(pcie->node, "msi-parent", 0);
 	if (!msi_node)
@@ -528,24 +388,18 @@ static int thunder_pcie_msi_enable(struct thunder_pcie *pcie,
 	if (!pcie->msi)
 		return -ENODEV;
 
-#ifdef CONFIG_KVM_ARM_VGIC
-
-	vits_msi = vgic_its_get_msi_node(bus, pcie->msi);
-
-
 	pcie->msi->dev = pcie->dev;
-	bus->msi = vits_msi;
-#else
-
 	bus->msi = pcie->msi;
-#endif
 
 	return 0;
 }
 
 static void thunder_pcie_config(struct thunder_pcie *pcie, u64 addr)
 {
+	void __iomem *gser_base;
+
 	atomic_set(&thunder_pcie_ecam_probed, 1);
+	set_its_pci_requester_id(thunder_pci_requester_id);
 
 	pcie->valid = true;
 
@@ -576,18 +430,33 @@ static void thunder_pcie_config(struct thunder_pcie *pcie, u64 addr)
 		break;
 	default:
 		pcie->valid = false;
+		return;
 	}
 
-	if (gser_base0 == NULL)
-		gser_base0 = devm_ioremap(pcie->dev,
-					 THUNDER_GSER_N0_BASE,
-					 THUNDER_GSER_SIZE);
-#ifdef CONFIG_NUMA
-	if (gser_base1 == NULL)
-		gser_base1 = devm_ioremap(pcie->dev,
-					 THUNDER_GSER_N1_BASE,
-					 THUNDER_GSER_SIZE);
-#endif
+	if (pcie->ecam < 4) {
+		if (gser_base0 == NULL) {
+			gser_base = devm_ioremap(pcie->dev,
+						 THUNDER_GSER_N0_BASE,
+						 THUNDER_GSER_SIZE);
+			if (IS_ERR(gser_base))
+				goto err;
+			gser_base0 = gser_base;
+		}
+	} else {
+		if (gser_base1 == NULL) {
+			gser_base = devm_ioremap(pcie->dev,
+						 THUNDER_GSER_N1_BASE,
+						 THUNDER_GSER_SIZE);
+			if (IS_ERR(gser_base))
+				goto err;
+			gser_base1 = gser_base;
+		}
+	}
+	return;
+err:
+	dev_err(pcie->dev,
+		"Unable to map gser_base: ret=%d\n", (int)PTR_ERR(gser_base));
+	return;
 }
 
 static int thunder_pcie_probe(struct platform_device *pdev)
@@ -595,8 +464,7 @@ static int thunder_pcie_probe(struct platform_device *pdev)
 	struct thunder_pcie *pcie;
 	struct resource *cfg_base;
 	struct pci_bus *bus;
-	int ret;
-	int primary_bus = 0;
+	int ret = 0;
 	LIST_HEAD(res);
 
 	pcie = devm_kzalloc(&pdev->dev, sizeof(*pcie), GFP_KERNEL);
@@ -612,29 +480,20 @@ static int thunder_pcie_probe(struct platform_device *pdev)
 	thunder_pcie_config(pcie, cfg_base->start);
 
 	pcie->cfg_base = devm_ioremap_resource(&pdev->dev, cfg_base);
-	if (IS_ERR(pcie->cfg_base) || IS_ERR(gser_base0)) {
+	if (IS_ERR(pcie->cfg_base)) {
 		ret = PTR_ERR(pcie->cfg_base);
 		goto err_ioremap;
 	}
 
-	pr_err("%s: ECAM%d CFG BASE 0x%llx gser_base0:%llx\n", __func__,
-	       pcie->ecam, (u64)cfg_base->start, (u64)gser_base0);
+	dev_info(&pdev->dev, "ECAM%d CFG BASE 0x%llx\n",
+		 pcie->ecam, (u64)cfg_base->start);
 
-#ifdef CONFIG_NUMA
-	if (IS_ERR(gser_base1)) {
-		ret = PTR_ERR(pcie->cfg_base);
-		goto err_ioremap;
-	}
-	pr_err("%s: ECAM%d CFG BASE 0x%llx gser_base1:%llx\n", __func__,
-	       pcie->ecam, (u64)cfg_base->start, (u64)gser_base1);
-#endif
 	ret = of_pci_get_host_bridge_resources(pdev->dev.of_node,
 					0, 255, &res, NULL);
 	if (ret)
 		goto err_root_bus;
 
-	bus = pci_create_root_bus(&pdev->dev, primary_bus, &thunder_pcie_ops,
-				pcie, &res);
+	bus = pci_create_root_bus(&pdev->dev, 0, &thunder_pcie_ops, pcie, &res);
 	if (!bus) {
 		ret = -ENODEV;
 		goto err_root_bus;
@@ -643,7 +502,8 @@ static int thunder_pcie_probe(struct platform_device *pdev)
 	/* Set reference to MSI chip */
 	ret = thunder_pcie_msi_enable(pcie, bus);
 	if (ret) {
-		pr_err("%s: Unable to set reference to MSI chip: ret=%d\n", __func__, ret);
+		dev_err(&pdev->dev,
+			"Unable to set reference to MSI chip: ret=%d\n", ret);
 		goto err_msi;
 	}
 
@@ -783,5 +643,5 @@ DECLARE_ACPI_MCFG_FIXUP("CAVIUM", "THUNDERX", thunder_acpi_mcfg_fixup);
 #endif
 
 MODULE_AUTHOR("Sunil Goutham");
-MODULE_DESCRIPTION("Cavium Thunder PCIe host controller driver");
+MODULE_DESCRIPTION("Cavium Thunder ECAM host controller driver");
 MODULE_LICENSE("GPL v2");
